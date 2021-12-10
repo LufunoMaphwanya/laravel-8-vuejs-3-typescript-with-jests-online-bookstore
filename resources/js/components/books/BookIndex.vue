a87af35641371fe6402251971bdab7afe286f30f
Lufuno Maphwanya <lcmaphwanya@gmail.com>
2021-12-10 22:55:32 +0200

    Adds books test seeder and vuejs components for index and showOne.

--- resources/js/components/books/BookIndex.vue
+++ resources/js/components/books/BookIndex.vue
@@ -1,3 +1,28 @@
 <template>
-    Our library: All books
+    <div class="container">
+        <div class="card" style="width: 18rem; float: left; margin: 5px" v-for="book in books" :key="book.id">
+            <router-link :to="{ name: 'books.show' , params: { id: book.id }}">
+                <div class="card-body">
+                        <h5 class="card-title">{{ book.title}}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">{{ book.year}}</h6>
+                </div>
+            </router-link>
+        </div>
+    </div>
 </template>
+
+<script>
+import useBooks from '../../composables/books';
+import { onMounted } from 'vue';
+
+export default {
+    setup() {
+        const { books, getBooks } = useBooks()
+        onMounted(getBooks)
+
+        return {
+            books,
+        }
+    }
+}
+</script>
--- resources/js/components/books/BookShow.vue
+++ resources/js/components/books/BookShow.vue
@@ -1,3 +1,41 @@
 <template>
-    Read book
+    <div class="container">
+        <div class="row">
+            <div class="col-3">
+                <div class="card" style="width: 90%;  margin: 5px">
+                    <div class="card-body">
+                            <h5 class="card-title">{{ book.title}}</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">{{ book.year}}</h6>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-9 border">
+            </div>
+        </div>
+    </div>
 </template>
+
+<script>
+import useBooks from '../../composables/books'
+import { onMounted } from 'vue';
+
+export default {
+    props: {
+        id: {
+            required: true,
+            type: String
+        }
+    },
+
+    setup(props) {
+        const { errors, book, getBook } = useBooks()
+
+        onMounted(() => getBook(props.id))
+
+        return {
+            book
+        }
+    }
+}
+</script>
